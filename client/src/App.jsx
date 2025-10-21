afcd31398b740b5c6ec2b1b2ace32b9d78b7948e
Valentine Wanjiru <kt116tabitha@gmail.com>
2025-10-21 20:00:43 +0300

    Installed the required dependencies for both the client and server sides

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <div>
+
+        <h1 className="text-5xl font-bold underline text-white">
+          Hello world!
+        </h1>
+    </div>
+  )
+}
+
+export default App
